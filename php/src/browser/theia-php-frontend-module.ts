68b02f7cc155656f26b0fdcccfa954a3bc3b88c8
Victor Rubezhny <vrubezhny@redhat.com>
2019-04-03 18:32:51 +0200

    Remove PHP grammar contribution
    PHP grammar contributions are added to Theia for PHP Language in [1],
    as such they must be removed from theia-php-extension.
    
    [1] https://github.com/theia-ide/theia/issues/4582
    
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>

--- php/src/browser/theia-php-frontend-module.ts
+++ php/src/browser/theia-php-frontend-module.ts
@@ -5,14 +5,10 @@
 import { PHPClientContribution } from './theia-php-contribution';
 import { LanguageClientContribution } from "@theia/languages/lib/browser";
 import { ContainerModule } from "inversify";
-import { LanguageGrammarDefinitionContribution } from '@theia/monaco/lib/browser/textmate';
-import { PhpGrammarContribution } from './php-grammar-contribution';
 
 export default new ContainerModule(bind => {
     // add your contribution bindings here
-    
+
     bind(PHPClientContribution).toSelf().inSingletonScope();
     bind(LanguageClientContribution).toDynamicValue(ctx => ctx.container.get(PHPClientContribution));
-
-    bind(LanguageGrammarDefinitionContribution).to(PhpGrammarContribution).inSingletonScope();
 });
